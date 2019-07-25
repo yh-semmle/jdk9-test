8704ce61fe99745e5a7f6a4778c94f32b20e1161
yh-semmle <yh@semmle.com>
2019-07-24 22:06:21 -0400

    Add script to clone `jdk-9.0.4+12` for testing purposes.

---
+++ get-source-jdk9.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eux
+
+hg clone http://hg.openjdk.java.net/jdk-updates/jdk9u
+cd jdk9u
+hg checkout 1b1226687b89 # jdk-9.0.4+12
+chmod u+x get_source.sh
+./get_source.sh
+chmod u+x configure
+export DISABLE_HOTSPOT_OS_VERSION_CHECK=ok
+
