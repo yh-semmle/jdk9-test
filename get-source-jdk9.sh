ca2d3e8458fc1cdd898d1ed87a451b9a120f0a99
yh-semmle <43018487+yh-semmle@users.noreply.github.com>
2020-03-21 12:02:53 -0400

    Make commit SHA for `get-source-jdk9.sh` configurable

--- get-source-jdk9.sh
+++ get-source-jdk9.sh
@@ -2,9 +2,11 @@
 
 set -eux
 
+SHA=$1
+
 hg clone http://hg.openjdk.java.net/jdk-updates/jdk9u
 cd jdk9u
-hg checkout 1b1226687b89 # jdk-9.0.4+12
+hg checkout $SHA
 chmod u+x get_source.sh
 ./get_source.sh
 chmod u+x configure
